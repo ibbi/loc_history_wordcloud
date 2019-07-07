e03d184647a347a7aaab6a86ae470afc74b9bab0
ibbi <hi@ibbi.io>
2019-07-07 16:33:50 -0700

    messing with masks

--- generate_wordcloud/generate_wordcloud.py
+++ generate_wordcloud/generate_wordcloud.py
@@ -5,17 +5,24 @@ import re
 import json
 from PIL import Image
 from os import path
-from wordcloud import WordCloud
+from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 
 with open('../data/freq_dict.json') as json_file:
     freqDict = json.load(json_file)
 
-world_mask = np.array(Image.open("mask.png"))
-wc = WordCloud(background_color="white", max_words=1000, mask=world_mask)
+
+mask = np.array(Image.open("circle.png"))
+
+wc = WordCloud(background_color="white", max_words=1000,
+               mask=mask, contour_width=3, contour_color='steelblue')
 
 
 wc.generate_from_frequencies(freqDict)
-plt.imshow(wc, interpolation="bilinear")
+# print(len(freqDict))
+plt.imshow(wc, interpolation='bilinear')
+plt.axis("off")
+plt.figure()
+plt.imshow(mask, cmap=plt.cm.gray, interpolation='bilinear')
 plt.axis("off")
 plt.show()
