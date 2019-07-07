491abbe7eb7b6280c605bacf3b9ffa99944c27bb
ibbi <hi@ibbi.io>
2019-07-07 14:23:31 -0700

    generate wordcloud

--- generate_wordcloud/generate_wordcloud.py
+++ generate_wordcloud/generate_wordcloud.py
@@ -2,14 +2,18 @@ import numpy as np
 
 import os
 import re
+import json
 from PIL import Image
 from os import path
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
-from '../data/cityDict.py' import cityDict as freqDict
+
+with open('../data/freq_dict.json') as json_file:
+    freqDict = json.load(json_file)
 
 world_mask = np.array(Image.open("mask.png"))
 wc = WordCloud(background_color="white", max_words=1000, mask=world_mask)
+
 
 wc.generate_from_frequencies(freqDict)
 plt.imshow(wc, interpolation="bilinear")
