f50fb81a7b40ba8de73db20ed10933b35f2cba3e
ibbi <hi@ibbi.io>
2019-07-07 17:05:09 -0700

    masks working well, white bg

--- generate_wordcloud/generate_wordcloud.py
+++ generate_wordcloud/generate_wordcloud.py
@@ -5,7 +5,7 @@ import re
 import json
 from PIL import Image
 from os import path
-from wordcloud import WordCloud, STOPWORDS
+from wordcloud import WordCloud, ImageColorGenerator
 import matplotlib.pyplot as plt
 
 with open('../data/freq_dict.json') as json_file:
@@ -13,16 +13,21 @@ with open('../data/freq_dict.json') as json_file:
 
 
 mask = np.array(Image.open("circle.png"))
+image_colors = ImageColorGenerator(mask)
 
 wc = WordCloud(background_color="white", max_words=1000,
-               mask=mask, contour_width=3, contour_color='steelblue')
+               mask=mask)
 
 
 wc.generate_from_frequencies(freqDict)
 # print(len(freqDict))
-plt.imshow(wc, interpolation='bilinear')
+plt.imshow(wc.recolor(color_func=image_colors), interpolation="bilinear")
 plt.axis("off")
 plt.figure()
 plt.imshow(mask, cmap=plt.cm.gray, interpolation='bilinear')
 plt.axis("off")
 plt.show()
+
+
+image_colors = ImageColorGenerator(mask)
+# plt.figure(figsize=[7, 7])
