4b5ce5905efb6f6ddc3278e8b19e6e065b08f84c
ibbi <hi@ibbi.io>
2019-07-07 20:25:54 -0700

    adjust weight formula

--- reverse_geocode/find_cities.py
+++ reverse_geocode/find_cities.py
@@ -13,10 +13,11 @@ with open('../data/clean_history.json') as json_file:
             reverseInfo = reverse_geocode.search(coordinate)[0]
             reverseCity = reverseInfo['city']
             reverseCountry = reverseInfo['country']
+            # Adjust to change weight range
             if reverseCity in cityDict:
-                cityDict[reverseCity] += 1
+                cityDict[reverseCity] += 100/(cityDict[reverseCity]**2)
             else:
-                cityDict[reverseCity] = 1
+                cityDict[reverseCity] = 10
 
 with open('../data/freq_dict.json', 'w') as fp:
     json.dump(cityDict, fp)
