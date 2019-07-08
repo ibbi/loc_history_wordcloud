49594bd14fcd0fb942a8575c0808d8163aec0cb9
ibbi <hi@ibbi.io>
2019-07-07 17:48:07 -0700

    oop

--- generate_wordcloud/generate_wordcloud.py
+++ generate_wordcloud/generate_wordcloud.py
@@ -15,18 +15,12 @@ with open('../data/freq_dict.json') as json_file:
 mask = np.array(Image.open("mask.png"))
 image_colors = ImageColorGenerator(mask)
 
-wc = WordCloud(height=5000, width=10000, background_color="white", max_words=1000,
+wc = WordCloud(background_color="white", max_words=1000,
                mask=mask)
 
 
 wc.generate_from_frequencies(freqDict)
-plt.figure(figsize=(200, 100))
 
-# print(len(freqDict))
-plt.imshow(wc.recolor(color_func=image_colors), interpolation="bilinear")
+# plt.imshow(wc.recolor(color_func=image_colors), interpolation="bilinear")
 
 wc.to_file('thing.png')
-
-
-plt.axis("off")
-plt.figure()
